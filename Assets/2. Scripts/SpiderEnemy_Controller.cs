a944ff5dc8aa52013870bc71929ace27ec8144c8
CrixDeveloper <Christiand.Tortorellar@gmail.com>
2022-01-20 18:41:41 -0300

    07. Making - NavMesh Path.

--- Assets/2. Scripts/SpiderEnemy_Controller.cs
+++ Assets/2. Scripts/SpiderEnemy_Controller.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class SpiderEnemy_Controller : MonoBehaviour
 {
+    #region Variables to use: 
+
+    // Private Variables: 
+    protected AudioSource spiderAS;
+
+    [Header("References:")]
+    public NavMeshAgent spiderAgent;
+    public Transform playerTarget;
+    public AudioClip playerHurt;
+
+    #endregion
+
+    #region Frame Dependent Methods: 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spiderAS = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        FollowPlayer();
     }
+
+    #endregion
+
+    #region Main Methods: 
+
+    private void FollowPlayer()
+    {
+        spiderAgent.GetComponent<NavMeshAgent>();
+        playerTarget.GetComponent<Transform>();
+
+        spiderAgent.destination = playerTarget.transform.position;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            spiderAS.PlayOneShot(playerHurt);
+        }
+    }
+
+    #endregion
 }
